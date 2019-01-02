6ee9b111526716a37df7ec23b2b85257b956c3ea
Christian Eder <christian.eder@zuehlke.com>
2019-01-02 16:01:58 +0100

    initial version of console app

---
+++ Heic2Jpg/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Linq;
+using ImageMagick;
+using ShellProgressBar;
+
+namespace Heic2Jpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args.Any() ? args.Single() : Directory.GetCurrentDirectory();
+            var heics = Directory.GetFiles(path, "*.heic", SearchOption.AllDirectories);
+            using (var progressBar = new ProgressBar(heics.Length, "Converting .heic files to .jpg"))
+            {
+                foreach (string heic in heics)
+                {
+                    using (var image = new MagickImage(heic))
+                    {
+                        image.Write(heic.Replace(Path.GetExtension(heic), ".jpg"));
+                    }
+
+                    try
+                    {
+                        File.Delete(heic);
+                    }
+                    catch
+                    {
+                    }
+
+                    progressBar.Tick();
+                }
+            }
+        }
+    }
+}
